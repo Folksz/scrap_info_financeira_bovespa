5c0c296d388a07987c1d89c225426c49f3318607
Folksz <pedrohen.fc@gmail.com>
2021-03-17 23:07:35 -0300

    correcao e separacao dos dados

--- scrap_DRE_BPA_BPP.R
+++ scrap_DRE_BPA_BPP.R
@@ -31,7 +31,6 @@ source("dre.R")
 
 
 #Mostra todas as Empresas
-empresas=sort(listar_empresas())
 
 
 anos=c(2011,2012,2013,2014,2015,2016,2017,2018,2019,2020)
@@ -43,26 +42,27 @@ lista_dfp_TODO=baixar_excel_porano(anos,sufixos_desejaveis,"dfp")
 
 #Agora escolha de uma soh empresa pra ficar mais facil e paupavel, mas guarda a lista todos pra caso queira usar de novo
 
-empresas_analisadas=c("RAIA DROGASIL S.A.")
-lista_itr=filtrar_empresas_desejadas(lista_itr_TODO,empresas_analisadas)
-lista_dfp=filtrar_empresas_desejadas(lista_dfp_TODO,empresas_analisadas)
+#Lista de empresas
+empresas=sort(listar_empresas())
+empresa_analisada=c("RAIA DROGASIL S.A.")
+
+# Captura os itrs e dfps
+lista_itr=filtrar_empresas_desejadas(lista_itr_TODO,empresa_analisada)
+lista_dfp=filtrar_empresas_desejadas(lista_dfp_TODO,empresa_analisada)
 
 
+#Construcao de Ativo, Passivo e DRE
 ativo=construir_balanco(lista_itr[['itr_cia_aberta_BPA_con']],lista_dfp[['dfp_cia_aberta_BPA_con']])
-ativo_prova=ativo%>%filter(dte_data>as.Date('2019-08-01'))%>%filter(conta %in% c("1","1.01","1.02","1.01.01","1.01.08.03.03"))
-k=ativo_prova%>%dcast(descricao~dte_data,value.var="valor")
-
 passivo=construir_balanco(lista_itr[['itr_cia_aberta_BPP_con']],lista_dfp[['dfp_cia_aberta_BPP_con']])
-passivo_prova=passivo%>%filter(dte_data>as.Date('2019-08-01'))%>%filter(conta %in% c("2","2.01","2.02","2.03","2.03.01","2.03.02.05"))
-k=passivo_prova%>%dcast(descricao~dte_data,value.var="valor")
-
-
-k=lista_itr[['itr_cia_aberta_DRE_con']]
 dre=construir_DRE(lista_itr[['itr_cia_aberta_DRE_con']],lista_dfp[['dfp_cia_aberta_DRE_con']])
 
+#Caso se Queira Analisar soh a ponta
+ativo_prova=ativo%>%filter(dte_data>as.Date('2019-08-01'))%>%filter(conta %in% c("1","1.01","1.02","1.01.01","1.01.08.03.03"))
+k=ativo_prova%>%dcast(descricao~dte_data,value.var="valor")
+passivo_prova=passivo%>%filter(dte_data>as.Date('2019-08-01'))%>%filter(conta %in% c("2","2.01","2.02","2.03","2.03.01","2.03.02.05"))
+k=passivo_prova%>%dcast(descricao~dte_data,value.var="valor")
 dre_prova=dre%>%filter(dte_data>as.Date('2019-08-01'))%>%filter(conta %in% c("3.01"))
 k=dre_prova%>%dcast(descricao~dte_data,value.var="valor")
 
 
 
-ativo%>%filter(CD_CONTA  %in% c("1","1.01"))
